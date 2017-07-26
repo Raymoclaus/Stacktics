197566a4bbccc703b2d84d40e20db7e3b32d76d8
LucasMayor <lucasm@goldenagecreative.com.au>
2017-07-26 23:00:47 +1000

    Basic map creation and camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	//reference to the camera component
+	private Camera cam;
+
+	//Define camera modes
+	public enum CameraMode {Orthographic, LockedPerspective, FreePerspective};
+	private CameraMode camMode = CameraMode.Orthographic;
+	public CameraMode CamMode
+	{
+		get { return camMode; }
+		private set { camMode = value; }
+	}
+
+	//transition-related stuff
+	private bool IsTransitioning
+	{
+		get { return fadingIn || fadingOut; }
+	}
+	private bool fadingOut, fadingIn;
+	public float fadeTime;
+	private float fadeCounter = 0f;
+	private Vector2 nearFarClippingLimits = new Vector2();
+
+	//reference to camera target
+	public Transform target;
+
+	//orthographic scrolling
+	public float orthoScrollPadding, orthoScrollLimit;
+	public Vector2 orthoZoomLimits;
+	public bool smoothZooming;
+	private float zoomTo, zoomCount = 0f, zoomTime = 1f;
+	public Vector3 orthographicCenter = new Vector3(0f, 45f, 0f);
+
+
+	void Start()
+	{
+		cam = GetComponent<Camera>();
+		//get near and far clipping planes
+		nearFarClippingLimits.x = cam.nearClipPlane;
+		nearFarClippingLimits.y = cam.farClipPlane;
+		zoomTo = cam.orthographicSize;
+	}
+
+	void Update()
+	{
+		//only run update method if not in the middle of a transition
+		if (CheckTransition())
+		{
+			return;
+		}
+
+		switch(camMode)
+		{
+		case CameraMode.Orthographic:
+			{
+				UpdateOrthographic();
+				break;
+			}
+		case CameraMode.LockedPerspective:
+			{
+				UpdateLocked();
+				break;
+			}
+		case CameraMode.FreePerspective:
+			{
+				UpdateFree();
+				break;
+			}
+		}
+	}
+
+	private bool CheckTransition()
+	{
+		//if already in a transition then return true
+		if (IsTransitioning)
+		{
+			return true;
+		}
+		//if player is trying to start a transition then start it and return true
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			StartCoroutine(FadeOut());
+			return true;
+		}
+		//otherwise return false
+		return false;
+	}
+
+	private void UpdateOrthographic()
+	{
+		CheckOrthographicScrolling();
+		CheckOrthographicZooming();
+	}
+
+	private void CheckOrthographicScrolling()
+	{
+		Vector2 mousePos = Input.mousePosition;
+		Vector3 direction = Vector3.zero;
+		/* if the cursor is near the edges of the screen then scroll camera in that direction */
+		//check left side of the screen
+		if (mousePos.x < orthoScrollPadding)
+		{
+			direction.x += -1f;
+			direction.z += 1f;
+		}
+		//check right side of the screen
+		if (mousePos.x > Screen.width - orthoScrollPadding)
+		{
+			direction.x += 1f;
+			direction.z += -1f;
+		}
+		//check top side of the screen
+		if (mousePos.y > Screen.height - orthoScrollPadding)
+		{
+			direction.x += 1f;
+			direction.z += 1f;
+		}
+		//check bottom side of the screen
+		if (mousePos.y < orthoScrollPadding)
+		{
+			direction.x += -1f;
+			direction.z += -1f;
+		}
+		//apply direction to camera's position
+		direction.Normalize();
+		transform.position += direction * cam.orthographicSize / 30f;
+		//check to see if camera has reached scroll limit
+		while (Vector3.Distance(orthographicCenter, transform.position) > orthoScrollLimit)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, orthographicCenter, 0.1f);
+		}
+	}
+
+	private void CheckOrthographicZooming()
+	{
+		//check to see if the mouse wheel is being used
+		float mouseScroll = -Input.mouseScrollDelta.y;
+		if (mouseScroll != 0)
+		{
+			//reset zoom Count to allow for smooth scrolling
+			zoomCount = 0;
+			//only apply the scroll if the zoom would remain within limits
+			if (zoomTo + mouseScroll >= orthoZoomLimits.x &&
+				zoomTo + mouseScroll <= orthoZoomLimits.y)
+			{
+				zoomTo += mouseScroll;
+			}
+		}
+		//increment zoom counter for smooth scrolling
+		zoomCount += Time.deltaTime;
+		//if smooth zooming is enabled then gradually zoom to calculated orthographic size
+		if (smoothZooming)
+		{
+			cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomTo, zoomCount / zoomTime);
+		}
+		//otherwise just set the zoom to what it should be
+		else
+		{
+			cam.orthographicSize = zoomTo;
+		}
+	}
+
+	private void UpdateLocked()
+	{
+		
+	}
+
+	private void UpdateFree()
+	{
+		
+	}
+
+	public void SetMode(CameraMode mode)
+	{
+		camMode = mode;
+		switch (camMode)
+		{
+		case CameraMode.Orthographic:
+			{
+				cam.orthographic = true;
+				break;
+			}
+		case CameraMode.LockedPerspective:
+			{
+				cam.orthographic = false;
+				break;
+			}
+		case CameraMode.FreePerspective:
+			{
+				cam.orthographic = false;
+				break;
+			}
+		}
+	}
+
+	private IEnumerator FadeOut()
+	{
+		//start fading out
+		fadingOut = true;
+		//loop until the clipping planes match after a certain amount of time
+		while (fadeCounter < fadeTime / 2f)
+		{
+			//increment counter
+			fadeCounter += Time.deltaTime;
+			//adjust camera's far clip plane
+			cam.farClipPlane = Mathf.Lerp(cam.farClipPlane, nearFarClippingLimits.x * 1.01f, fadeCounter / (fadeTime / 2f));
+			//wait until end of frame before continuing loop
+			yield return 0;
+		}
+		//reset counter, change camera mode and start fading back in
+		fadeCounter = 0;
+		fadingIn = true;
+		fadingOut = false;
+		StartCoroutine(FadeIn());
+		//check which mode to set to
+		if (cam.orthographic)
+		{
+			if (target != null)
+			{
+				SetMode(CameraMode.LockedPerspective);
+			}
+			else
+			{
+				SetMode(CameraMode.FreePerspective);
+			}
+		}
+		else
+		{
+			SetMode(CameraMode.Orthographic);
+		}
+	}
+
+	private IEnumerator FadeIn()
+	{
+		//loop until the clipping planes match after a certain amount of time
+		while (fadeCounter < fadeTime / 2f)
+		{
+			//increment counter
+			fadeCounter += Time.deltaTime;
+			//adjust camera's far clip plane
+			cam.farClipPlane = Mathf.Lerp(cam.farClipPlane, nearFarClippingLimits.y, fadeCounter / (fadeTime / 2f));
+			//wait until end of frame before continuing loop
+			yield return 0;
+		}
+		//reset counter and start fading back in
+		fadeCounter = 0;
+		fadingIn = false;
+	}
+}
---
+++ Assets/Scripts/MapReader.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This class places tiles when the level starts */
+public class MapReader : MonoBehaviour
+{
+	/* Variable Declaration */
+	#region
+	//reference to land tile
+	public Transform landTile;
+	//nested list of coordinates to place tiles. Each Vector2 determines vertical size and vertical offset
+	//coords[vertical level (y)][horizontal level (x)][z inset]
+	private List<List<List<Vector2>>> coords = new List<List<List<Vector2>>>();
+	//references to all the tiles created by this class
+	private List<List<List<Transform>>> tiles = new List<List<List<Transform>>>();
+	//store the width/height/depth of the landTile prefab
+	private Vector3 tileSize;
+	//center of the map
+	public Vector3 Center { get { return new Vector3(coords[0].Count / 2f * tileSize.x, 0f, coords[0][0].Count / 2f * tileSize.z); } }
+	#endregion
+
+
+	void Start()
+	{
+		//get the size of the tile prefab
+		tileSize = landTile.localScale;
+		//if no coordinates are provided create a basic default map of coordinates
+		CreateDefaultCoordinates ();
+		//once the coords have been filled out, instantiate copies of the landTile at each coordinate
+		CreateLandTiles();
+	}
+
+	/* This creates a flat 10x10 plane of coordinates with 1 floor */
+	private void CreateDefaultCoordinates()
+	{
+		//some arbitrary values for creating a default level
+		int levels = 2, width = 10, length = 10;
+		//default vertical size of 1 and vertical offset of 0
+		Vector2 defaultProperties = Vector2.right;
+
+		for (int i = 0; i < levels; i++)
+		{
+			coords.Add(new List<List<Vector2>>());
+			for (int j = 0; j < width; j++)
+			{
+				coords[i].Add(new List<Vector2>());
+				for (int k = 0; k < length; k++)
+				{
+					coords[i][j].Add(defaultProperties);
+				}
+			}
+		}
+	}
+
+	/* Creates copies of the tile prefab at each coordinate */
+	private void CreateLandTiles()
+	{
+		for (int i = 0; i < coords.Count; i++)
+		{
+			tiles.Add(new List<List<Transform>>());
+			for (int j = 0; j < coords[i].Count; j++)
+			{
+				tiles[i].Add(new List<Transform>());
+				for (int k = 0; k < coords[i][j].Count; k++)
+				{
+					//Determine vertical size and vertical offset of current individual tile coordinate
+					Vector3 individualSize = tileSize;
+					individualSize.y = coords[i][j][k].x;
+					float verticalOffset = coords[i][j][k].y;
+					//create new tile
+					Transform newTile = Instantiate(landTile);
+					//set the x position (j), y position (i = floor) and z position (k)
+					newTile.position = new Vector3(
+						j * individualSize.x + individualSize.x,
+						individualSize.y / 2f + verticalOffset,
+						k * individualSize.z + individualSize.z);
+					//store new tile in the tiles nested list
+					tiles[i][j].Add(newTile);
+				}
+			}
+		}
+	}
+}
